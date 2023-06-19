d58dcfff0fdafe39b682c06b190edbc48f902f6e
yangshihao <yangshihao@bhb.tv>
2023-06-19 15:17:05 +0800

    fix:去除max-tokens限制

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "af6e8931fa326a3a"; }
+/******/ 		__webpack_require__.h = function() { return "2d99823b9b15f47b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Analysis/Analysis.tsx
+++ components/Analysis/Analysis.tsx
@@ -6,44 +6,54 @@ import styles from './index.module.scss';
 export default function Analysis() {
   const [analysis, setAnalysis] = useState(null);
   const [assistantResponse, setAssistantResponse] = useState('');
+  const [error, setError] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch('/api/openai', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          messages: [
-            {
-              role: 'system',
-              content:
-                '你是一位账单流水理财助手，专门分析财务数据并提供深入的信息。',
-            },
-            {
-              role: 'user',
-              content: `这是我上个月的财务数据：${JSON.stringify(
-                dataStore.sheetData.slice(0, 20)
-              )}。请分析并提供一些见解：`,
-            },
-          ],
-        }),
-      });
+      try {
+        const response = await fetch('/api/openai', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            messages: [
+              {
+                role: 'system',
+                content:
+                  '你是一位账单流水理财助手，专门分析财务数据并提供深入的信息。',
+              },
+              {
+                role: 'user',
+                content: `这是我上个月的财务数据：${JSON.stringify(
+                  dataStore.sheetData.slice(0, 20)
+                )}。请分析并提供一些见解：`,
+              },
+            ],
+          }),
+        });
 
-      if (response.ok) {
-        const data = await response.json();
-        setAnalysis(data);
-        if (data.choices && data.choices.length > 0) {
-          setAssistantResponse(data.choices[0].message.content);
+        if (response.ok) {
+          const data = await response.json();
+          setAnalysis(data);
+          if (data.choices && data.choices.length > 0) {
+            setAssistantResponse(data.choices[0].message.content);
+          }
+        } else {
+          throw new Error('HTTP error, status = ' + response.status);
         }
-      } else {
-        console.error('HTTP error', response.status);
+      } catch (error) {
+        setError('网络错误');
+        console.error('Error:网络错误', error);
       }
     };
 
     fetchData();
   }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <div>
